f1edaeb30f47f369dc5218056069de275a84385c
Molnár József <mojoe@freemail.hu>
2019-12-08 14:28:43 +0100

    VersionSerializer added

---
+++ RemoteCommunication.Tests/VersionSerializerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace RemoteCommunication.Tests
+{
+    public class VersionSerializerTests
+    {
+        [Theory]
+        [MemberData(nameof(VersionData))]
+        public void Serializing(Version value)
+        {
+            var s = new VersionSerializer();
+
+            Assert.False(s.CanSerialize(s, null));
+            Assert.True(s.CanSerialize(new Version(1, 2, 3, 4), null));
+
+            using (var ms = new MemoryStream())
+            {
+                Assert.True(s.CanSerialize(value, null));
+                using (var bw = new BinaryWriter(ms, Encoding.UTF8, true))
+                    s.Serialize(value, bw, null);
+                ms.Position = 0;
+                using (var br = new BinaryReader(ms, Encoding.UTF8, true))
+                    Assert.Equal(value, s.Deserialize(br, null));
+            }
+        }
+
+        public static IEnumerable<object[]> VersionData => new List<object[]> { new[] { new Version() }, new[] { new Version(1, 2) }, new[] { new Version(6, 5, 4, 3) } };
+    }
+}
---
+++ RemoteCommunication/VersionSerializer.cs
@@ -0,0 +1,32 @@
+﻿namespace RemoteCommunication
+{
+    using System;
+    using System.IO;
+
+    public class VersionSerializer : ISerializer
+    {
+        public string Id => "ver";
+
+        public bool CanSerialize(object data, SerializationManager manager) => data is Version;
+
+        public object Deserialize(BinaryReader reader, SerializationManager manager)
+        {
+            var ma = reader.ReadInt32();
+            var mi = reader.ReadInt32();
+            var b = reader.ReadInt32();
+            var r = reader.ReadInt32();
+            if (b == -1)
+                return new Version(ma, mi);
+            return new Version(ma, mi, b, r);
+        }
+
+        public void Serialize(object data, BinaryWriter writer, SerializationManager manager)
+        {
+            var ver = (Version)data;
+            writer.Write(ver.Major);
+            writer.Write(ver.Minor);
+            writer.Write(ver.Build);
+            writer.Write(ver.Revision);
+        }
+    }
+}
